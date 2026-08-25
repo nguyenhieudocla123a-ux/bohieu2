b243762c56c2f20fcea7fed83a089cc29a2f2cba
Hieunguyen <nguyenhieudocla123a@gmail.com>
2026-08-25 23:12:44 +0700

    "Banner"

--- src/File2.java
+++ src/File2.java
@@ -1,5 +1,6 @@
 public class File2 {
     public static void main(String[] args) {
         System.out.println("Hiếu");
+        System.out.println("Chào mừng đến với bình nguyên vô tận");
     }
 }
